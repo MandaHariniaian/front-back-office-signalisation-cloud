d6b4e69f1cbe35d8e856a38804124a1f93357160
Manda <andriamitantsoamanda@gmail.com>
2022-01-20 11:07:13 +0100

    changement adresse serveur

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:9090",
+    baseURL: "https://rest-api-signalisation-cloud.herokuapp.com",
     headers: {
         "Content-type": "application/json" 
     }
